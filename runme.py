fc526e554d32277cb4a30f7166711952b2ffa011
Nikolaosdim <49408712+Nikolaosdim@users.noreply.github.com>
2019-04-12 16:08:16 +0200

    Update runme.py

--- runme.py
+++ runme.py
@@ -28,79 +28,35 @@ dt = 1  # length of each time step (unit: hours)
 
 # detailed reporting commodity/sites
 report_tuples = [
-    (2019, 'North', 'Elec'),
-    (2019, 'Mid', 'Elec'),
-    (2019, 'South', 'Elec'),
-    (2019, ['North', 'Mid', 'South'], 'Elec'),
-    (2024, 'North', 'Elec'),
-    (2024, 'Mid', 'Elec'),
-    (2024, 'South', 'Elec'),
-    (2024, ['North', 'Mid', 'South'], 'Elec'),
-    (2029, 'North', 'Elec'),
-    (2029, 'Mid', 'Elec'),
-    (2029, 'South', 'Elec'),
-    (2029, ['North', 'Mid', 'South'], 'Elec'),
-    (2034, 'North', 'Elec'),
-    (2034, 'Mid', 'Elec'),
-    (2034, 'South', 'Elec'),
-    (2034, ['North', 'Mid', 'South'], 'Elec'),    
+    (2050, 'Deutschland', 'Elec'),
     ]
-
-# optional: define names for sites in report_tuples
-report_sites_name = {('North', 'Mid', 'South'): 'All'}
 
 # plotting commodities/sites
 plot_tuples = [
-    (2019, 'North', 'Elec'),
-    (2019, 'Mid', 'Elec'),
-    (2019, 'South', 'Elec'),
-    (2019, ['North', 'Mid', 'South'], 'Elec'),
-    (2024, 'North', 'Elec'),
-    (2024, 'Mid', 'Elec'),
-    (2024, 'South', 'Elec'),
-    (2024, ['North', 'Mid', 'South'], 'Elec'),
-    (2029, 'North', 'Elec'),
-    (2029, 'Mid', 'Elec'),
-    (2029, 'South', 'Elec'),
-    (2029, ['North', 'Mid', 'South'], 'Elec'),
-    (2034, 'North', 'Elec'),
-    (2034, 'Mid', 'Elec'),
-    (2034, 'South', 'Elec'),
-    (2034, ['North', 'Mid', 'South'], 'Elec'),    
+    (2050, 'Deutschland', 'Elec'),  
     ]
-
-# optional: define names for sites in plot_tuples
-plot_sites_name = {('North', 'Mid', 'South'): 'All'}
 
 # plotting timesteps
 plot_periods = {
-    'all': timesteps[1:]
+    'all' timesteps[1:]
 }
 
 # add or change plot colors
 my_colors = {
-    'South': (230, 200, 200),
-    'Mid': (200, 230, 200),
-    'North': (200, 200, 230)}
+    'Deutschland'(230, 200, 200),
+    }
 for country, color in my_colors.items():
     urbs.COLORS[country] = color
 
 # select scenarios to be run
 scenarios = [
              urbs.scenario_base,
-             urbs.scenario_stock_prices,
-             urbs.scenario_co2_limit,
-             urbs.scenario_co2_tax_mid,
-             urbs.scenario_no_dsm,
-             urbs.scenario_north_process_caps,
-             urbs.scenario_all_together
-            ]
+			 urbs.scenario_co2_limit95,
+			 scenario_co2_nolimit]
 
 for scenario in scenarios:
     prob = urbs.run_scenario(input_files, solver, timesteps, scenario, 
                         result_dir, dt, objective, 
                         plot_tuples=plot_tuples,
-                        plot_sites_name=plot_sites_name,
                         plot_periods=plot_periods,
-                        report_tuples=report_tuples,
-                        report_sites_name=report_sites_name)
+                        report_tuples=report_tuples)
